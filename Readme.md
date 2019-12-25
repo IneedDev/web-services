1e5ff513faae9b3f5150be52a36a7d1ef5f7fc8b
paromaniuk <paromaniuk@inpost.pl>
2019-12-25 18:22:22 +0100

    clean up modules

---
+++ Readme.md
@@ -0,0 +1,7 @@
+#### Web Service
+
+http://127.0.0.1:9876/one?wsdl
+
+#### WebService Client
+
+call method from exposed wsdl 
--- web-service/src/com/company/TimeServer.java
+++ web-service/src/com/company/TimeServer.java
@@ -4,10 +4,8 @@ import javax.jws.WebMethod;
 import javax.jws.WebService;
 import javax.jws.soap.SOAPBinding;
 
-
-//Service Endpoint Interface
 @WebService
-@SOAPBinding(style = SOAPBinding.Style.RPC) // more on this later
+@SOAPBinding(style = SOAPBinding.Style.RPC)
 public interface TimeServer{
     @WebMethod
     String getTimeAsString();
